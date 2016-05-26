49ae452d8c13f2cc8888c89ac5d52a90f020622e
Vinh Nguyen <xuanvinhct@gmail.com>
2016-05-26 13:38:56 +0700

    logistic regression using stochastic method

--- logistic_regression/newton.rb
+++ logistic_regression/newton.rb
@@ -8,7 +8,7 @@ class Newton
     @data_y = read_data_file(file_y).map(&:first)
     @sample_size = data_x.size
     @learning_rate = 0.1
-    @theta = [0] * data_x.first.size
+    @theta = [0.0] * data_x.first.size
   end
 
   def execute(time)
@@ -23,11 +23,19 @@ class Newton
     end
   end
 
+  # def run
+  #   result = theta.map.with_index do |t, index| 
+  #     t + learning_rate * data_y.map.with_index{|y, i| (y - h_x(theta, data_x[i])) * data_x[i][index]}.reduce(&:+)
+  #   end
+  #   @theta = result
+  # end
+
   def run
-    result = theta.map.with_index do |t, index| 
-      t + learning_rate * data_y.map.with_index{|y, i| (y - h_x(theta, data_x[i])) * data_x[i][index]}.reduce(&:+)
+    theta.each_with_index do |t, index| 
+      binding.pry
+      new_t = t + learning_rate * data_y.map.with_index{|y, i| (y - h_x(theta, data_x[i])) * data_x[i][index]}.reduce(&:+)
+      @theta[index] = new_t
     end
-    @theta = result
   end
 
   def h_x(theta, x)
@@ -43,6 +51,7 @@ class Newton
   end
 end
 
-newton = Newton.new('x1.txt', 'y1.txt')
-newton.execute(20000)
+# newton = Newton.new('x1.txt', 'y1.txt')
+newton = Newton.new('logistic_x.txt', 'logistic_y.txt')
+newton.execute(5000)
 pp newton.theta
