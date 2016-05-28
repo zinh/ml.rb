281df6b8177fe42a168672745fed431aa7dc4d4e
Vinh Nguyen <xuanvinhct@gmail.com>
2016-05-28 17:03:36 +0700

    add newton method in logistic regression

--- logistic_regression/newton.rb
+++ logistic_regression/newton.rb
@@ -1,18 +1,11 @@
-require 'pp'
-require 'pry'
 class Newton
-  attr_reader :theta, :data_x, :data_y, :sample_size, :learning_rate
   def initialize(file_x, file_y)
     @data_x = read_data_file(file_x)
-    @data_x.map!{|a| a.unshift(0)}
+    @data_x.map!{|a| a.unshift(1.0)}
     @data_y = read_data_file(file_y).map(&:first)
     @sample_size = data_x.size
     @learning_rate = 0.1
-    @theta = [0.0] * data_x.first.size
-  end
-
-  def execute(time)
-    time.times{run}
+    @theta = [1.0] * data_x.first.size
   end
 
   private
@@ -22,20 +15,9 @@ class Newton
       line.strip.split(' ').map(&:to_f)
     end
   end
-
-  # def run
-  #   result = theta.map.with_index do |t, index| 
-  #     t + learning_rate * data_y.map.with_index{|y, i| (y - h_x(theta, data_x[i])) * data_x[i][index]}.reduce(&:+)
-  #   end
-  #   @theta = result
-  # end
-
-  def run
-    theta.each_with_index do |t, index| 
-      binding.pry
-      new_t = t + learning_rate * data_y.map.with_index{|y, i| (y - h_x(theta, data_x[i])) * data_x[i][index]}.reduce(&:+)
-      @theta[index] = new_t
-    end
+  
+  def derivative_l(theta, j)
+    data_y.map.with_index{|y, index| (y - h_x(theta, data_x[index])) * data_x[index][j]}.reduce(&:+)
   end
 
   def h_x(theta, x)
@@ -49,9 +31,10 @@ class Newton
   def g_z(z)
     1 / (1 + (Math::E)**(-z))
   end
-end
 
-# newton = Newton.new('x1.txt', 'y1.txt')
-newton = Newton.new('logistic_x.txt', 'logistic_y.txt')
-newton.execute(5000)
-pp newton.theta
+  def hessian_matrix
+  end
+
+  def hessian(i, j)
+  end
+end
